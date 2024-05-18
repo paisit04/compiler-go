7a8179b1cbeb49bac0f6e4ddd28d56736b17a81c
Paisit Jarunnamsiri <paisitj@Paisits-MacBook-Pro-14.local>
2024-05-18 13:49:19 -0700

    ch06

--- code/code.go
+++ code/code.go
@@ -36,6 +36,11 @@ const (
 
 	OpGetGlobal
 	OpSetGlobal
+
+	OpArray
+	OpHash
+
+	OpIndex
 )
 
 type Definition struct {
@@ -62,6 +67,9 @@ var definitions = map[Opcode]*Definition{
 	OpNull:          {"OpNull", []int{}},
 	OpGetGlobal:     {"OpGetGlobal", []int{2}},
 	OpSetGlobal:     {"OpSetGlobal", []int{2}},
+	OpArray:         {"OpArray", []int{2}},
+	OpHash:          {"OpHash", []int{2}},
+	OpIndex:         {"OpIndex", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
